beb1b2fb1ee2cc2646322832336ef68ca7e5d298
Eghasbp <eghaxperia@gmail.com>
2023-10-08 00:43:32 +0700

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "inter": ['inter'],
+        "Circular": ['Circular STD'],
+      }
+    },
   },
   plugins: [],
 }
